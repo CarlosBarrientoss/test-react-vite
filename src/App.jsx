4ca642646172e1a29595ea60c7d17a1c9d3cc734
CarlosBarrientoss <carlosabarrientoss@gmail.com>
2025-05-11 19:48:48 -0500

    Mi primer commit desde webStorm en un proyecto con React

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <article>
+            <p>Parrafo del articulo</p>
+        </article>
+        <section>
+            <p>Seccion de la pagina</p>
+        </section>
+        <footer>
+            c  Carlos Arturo Barrientos Suarez
+        </footer>
+
+    </>
+  )
+}
+
+export default App
